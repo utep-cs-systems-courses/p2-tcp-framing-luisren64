62b958f6f26de4aea60ec3bbde3d811a2dd51a38
luisren64 <lrenteria5@miners.utep.edu>
2021-03-09 23:05:23 -0700

    Initial commit. Testing if github works

---
+++ Lab/labClient.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+import socket
+import sys
+import re
+import time
+import os
+
+sys.path.append("../lib") #for params
+
+import params
+
---
+++ Lab/labServer.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+import socket
+import sys
+import re
+import os
+
+sys.path.append("../lib") #for params
+
+import params
