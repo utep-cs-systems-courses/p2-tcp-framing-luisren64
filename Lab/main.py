3c340a4e92e67181253b36bddd154bf050f5f7cc
luisren64 <lrenteria5@miners.utep.edu>
2021-03-19 23:54:43 -0600

    Readline complete, continuing work on file transfer

--- Lab/main.py
+++ Lab/main.py
@@ -12,9 +12,33 @@ def main():
         else:
             os.write(2, "$".encode())
 
-        #args = myReadline()
+        args = myReadline().split(" ") #Seperating into different words"
+
+        if args[0] == "exit" or if args[0] == "Exit":
+            os.write(2, "Exiting \n".encode())
+            sys.exit(0)
 
 
+#Method to read the input that will be sent as a message
 def myReadline():
     #code here
+    i = 0
+    inp = "" 
+    inbuff = read(0,100) #Making a buffer to read the input message to be sent.
+    temp = inbuff.decode()
+
+    while i < len(inp):
+        curr = temp[i] #temp variable for each char in the input string
+
+        #As long as we do not reach the newline, we keep adding chars
+        if curr != '\n':
+            inp += curr
+            i = i+1
+
+        #We've reached the newline, return inp
+        else:
+            print(inp) #TEST DELETE LATER
+            return inp
+
+    return ""
         
